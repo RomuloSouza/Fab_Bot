b95633e65a7b6465901a26fbbb3a3421fa0a649f
Cleber Castro <cjj.castro3@gmail.com>
2019-02-17 19:26:28 -0300

    Create new methods in botmanager

--- botmanager.py
+++ botmanager.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 import logging
 import db
+import constants
 from util import isfloat
 from db import Cart
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ForceReply
@@ -13,6 +14,10 @@ logger = logging.getLogger(__name__)
 
 
 class BotManager:
+    """
+    Define all commands used in the bot
+    """
+
     def start(self, bot, update):
         # keyboard = [[InlineKeyboardButton("Option 1", callback_data='1'),
         #             InlineKeyboardButton("Option 2", callback_data='2')],
@@ -27,6 +32,10 @@ class BotManager:
 
 
     def call_back(self, bot, update):
+        """
+        Represents an incoming callback query from a callback button in an inline keyboard.
+        """
+
         print(update)
         query = update.callback_query
         method, option = query.data.split(" ")
@@ -39,9 +48,21 @@ class BotManager:
             bot.edit_message_text(text="{} successfully added. You should {}".format(cart.name, cart.quantity),
                                 chat_id=query.message.chat_id,
                                 message_id=query.message.message_id)
+        elif(method == "rm"):
+            query_db = db.SESSION.query(Cart).filter_by(id=int(option))
+            cart = query_db.one()
+            db.SESSION.delete(cart)
+            db.SESSION.commit()
+
+            bot.edit_message_text(text="{} successfully removed.".format(cart.name),
+                                chat_id=query.message.chat_id,
+                                message_id=query.message.message_id)
 
     def list_cart(self, bot, update):
-        """List all products linked to chat"""
+        """
+        List all products linked to chat
+        """
+
         query = db.SESSION.query(Cart).filter_by(chat=update.message.chat.id)
         cart = query.all()
         text = "Products\n"
@@ -51,7 +72,10 @@ class BotManager:
         update.message.reply_text(text)
 
     def new_product(self, bot, update):
-        """Insert ap product into the database linked to chat"""
+        """
+        Insert a product into the database linked to chat
+        """
+
         text = update.message.text
         commands = text.split(" ")
         if(len(commands) >= 3 and isfloat(commands[-1])):
@@ -63,8 +87,21 @@ class BotManager:
             db.SESSION.commit()
             update.message.reply_text("Procuct succesfully added")
 
+    def remove_product(self, bot, update):
+        """
+        Remove a product into the database linked to chat
+        """
+
+        keyboard = self.create_rm_buttons(update.message.chat.id)
+        reply_markup = InlineKeyboardMarkup(keyboard)
+
+        update.message.reply_text("Please choose a product:", reply_markup=reply_markup)
+
     def fab_products(self, bot, update):
-        """Add all products used in FSW"""
+        """
+        Add all products used in FSW
+        """
+        
         # TODO - Add all products
         cart = Cart(chat=update.message.chat.id, name="Guarana", price="1.50", quantity=0)
         db.SESSION.add(cart)
@@ -72,26 +109,52 @@ class BotManager:
         update.message.reply_text("Products successfully added")
 
     def add_to_cart(self, bot, update):
-        """Add a product to the account"""
-        keyboard = self.create_buttons(update.message.chat.id)
+        """
+        Add a product to the account
+        """
+        
+        keyboard = self.create_add_buttons(update.message.chat.id)
         reply_markup = InlineKeyboardMarkup(keyboard)
 
         update.message.reply_text("Please choose a product:", reply_markup=reply_markup)
 
     def help(self, bot, update):
-        update.message.reply_text("Use /start to test this bot.")
+        """
+        Reply to user all commands used in the bot 
+        """
+
+        update.message.reply_text(constants.HELP)
 
 
     def error(self, bot, update, error):
-        """Log Errors caused by Updates."""
+        """
+        Log Errors caused by Updates.
+        """
+
         logger.warning('Update "%s" caused error "%s"', update, error)
 
-    def create_buttons(self, chat_id):
-        """Create inline keyboard buttons"""
+    def create_add_buttons(self, chat_id):
+        """
+        Create inline keyboard buttons to insert a cart
+        """
+
         query = db.SESSION.query(Cart).filter_by(chat=chat_id)
         products = query.all()
         keyboard_buttons = []
         for product in products:
             keyboard_buttons.append(InlineKeyboardButton(product.name, callback_data=("add " + str(product.id))))
+
+        return [keyboard_buttons[i:i+3] for i in range(0, len(keyboard_buttons), 3)]
+
+    def create_rm_buttons(self, chat_id):
+        """
+        Create inline keyboard buttons to remove product
+        """
+
+        query = db.SESSION.query(Cart).filter_by(chat=chat_id)
+        products = query.all()
+        keyboard_buttons = []
+        for product in products:
+            keyboard_buttons.append(InlineKeyboardButton(product.name,callback_data=("rm " + str(product.id))))
 
         return [keyboard_buttons[i:i+3] for i in range(0, len(keyboard_buttons), 3)]
--- constants.py
+++ constants.py
@@ -2,3 +2,12 @@ import os
 
 TOKEN = os.environ['BOT_API_TOKEN']
 URL_TELEGRAM = 'https://api.telegram.org/bot'+ str(TOKEN)
+HELP = """
+You can control me by sending these commands:
+ /list - list all products
+ /dept - list your depts and values
+ /add - add a product to dept
+ /pay - remove a product to dept
+ /newProd - create a new product
+ /rmProd - delete a product
+"""
